34b13784baa6c3caf6d48d619ab93ddb6a1de106
URDeVotee <hzheng10@u.rochester.edu>
2015-11-18 01:06:15 -0500

    IN AND OUT

--- static_files/js/survey.js
+++ static_files/js/survey.js
@@ -1,7 +1,93 @@
-//@Version 2.1
+//@Version 0.5
 
 var main = function() {
+	var age_select = false;
+	var occupation_select = false;
+	var gender_select = false;
 	
+	setTimeout(function() {
+		info_flyin();
+	}, 1500);
+	
+	$('.container-age .option').click(function(event){
+		if (!age_select) {
+			age_select = true;
+			var target = event.target;
+			$(target).css({
+				'background-color': 'rgb(102, 102, 102)',
+				'color': 'rgba(255, 255, 128, 0.95)'
+			});
+		}
+		else {
+			$('.container-age .option').css({
+				'background-color': 'rgba(217, 217, 217,0.4)',
+				'color': 'black'
+			});
+			var target = event.target;
+			$(target).css({
+				'background-color': 'rgb(102, 102, 102)',
+				'color': 'rgba(255, 255, 128, 0.95)'
+			});
+		}
+    });
+	
+	$('.container-occupation .option').click(function(event){
+		if (!occupation_select) {
+			occupation_select = true;
+			var target = event.target;
+			$(target).css({
+				'background-color': 'rgb(102, 102, 102)',
+				'color': 'rgba(255, 255, 128, 0.95)'
+			});
+		}
+		else {
+			$('.container-occupation .option').css({
+				'background-color': 'rgba(217, 217, 217,0.4)',
+				'color': 'black'
+			});
+			var target = event.target;
+			$(target).css({
+				'background-color': 'rgb(102, 102, 102)',
+				'color': 'rgba(255, 255, 128, 0.95)'
+			});
+		}
+    });
+	
+	$('.container-gender .option').click(function(event){
+		if (!gender_select) {
+			gender_select = true;
+			var target = event.target;
+			$(target).css({
+				'background-color': 'rgb(102, 102, 102)',
+				'color': 'rgba(255, 255, 128, 0.95)'
+			});
+		}
+		else {
+			$('.container-gender .option').css({
+				'background-color': 'rgba(217, 217, 217,0.4)',
+				'color': 'black'
+			});
+			var target = event.target;
+			$(target).css({
+				'background-color': 'rgb(102, 102, 102)',
+				'color': 'rgba(255, 255, 128, 0.95)'
+			});
+		}
+    });
+	
+	$('#submit').click(function(event){
+		info_flyout();
+    });
+}
+
+function info_flyin(){
+	$('.basic_info_ticket').animate({top: "28%"}, 1000);
+	$('.background').css({'-webkit-filter': 'blur(1px)'});
+}
+
+function info_flyout(){
+	$('.basic_info_ticket').animate({top: "150%"}, 1000);
+	$('.background').css({'-webkit-filter': 'blur(0px)'});
 }
 
 $(document).ready(main);
