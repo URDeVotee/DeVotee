94497c4cc3f802622e9e2caaa86a41d0558bdc09
URDeVotee <Sonny@dhcp-10-4-12-216.wireless.rochester.edu>
2015-11-20 19:50:52 -0500

    LOGOUT_3

--- static_files/js/Votee.js
+++ static_files/js/Votee.js
@@ -6,6 +6,26 @@ var main = function() {
 	$('.ReVote').click(function() {
 		window.location.href = "survey.html";
 	});
+
+	$('.Abstain').click(function() {
+		$.ajax({
+		    url: "users/logout",
+		    type: "GET",
+		    dataType: "json",
+
+		    success: function(data){
+		      if (typeof data.redirect == 'string'){
+		      	document.cookie = document.cookie + ";expires=Thu, 01 Jan 1970 00:00:00 GMT";
+		        window.location = data.redirect;
+		    }
+		      else
+		        console.log("not a string");
+		    },
+		    error: function(){
+		      console.log("direction error");
+		    }
+		});
+	});
 }
 
 function infoquery(){
@@ -20,6 +40,7 @@ function infoquery(){
 	     	$(".occupation p").html(data.occupation);
 	     	$(".gender p").html(data.gender);
 	    },
+
 	    error: function(){
 	      console.log("direction error");
 	    }
